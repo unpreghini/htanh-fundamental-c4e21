4c3fda8d9e6b62532e743a538deebe820dccefb0
Tuan Anh <anht.hoang158@gmail.com>
2018-09-06 00:01:17 +0700

    minor fix

--- Lab2/Assignments/itunes_download.py
+++ Lab2/Assignments/itunes_download.py
@@ -7,11 +7,10 @@ for song in songs:
     title = song['Title']
     artist = song['Artist']
     search = title + " " + artist
-    search_list = [search]
     options = {
         'default_search': 'ytsearch',
         'max_downloads': 1,
         'format': 'bestaudio/audio'
     }
     dl = YoutubeDL(options)
-    dl.download(search_list)
+    dl.download([search])
