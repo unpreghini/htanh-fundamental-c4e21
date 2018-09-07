321a36a037675899d5e8cb939a3967ed8ce873b6
Tuan Anh <anht.hoang158@gmail.com>
2018-09-07 18:16:04 +0700

    Change repo

--- Lab1/linear_search.py
+++
@@ -1,9 +0,0 @@
-numbers = [1, 3, -9, 2, 7, 10, -22]
-
-n = int(input("Enter your number: "))
-
-if n in numbers:
-    found_index = numbers.index(n)
-    print("Found", n, "at", found_index)
-else:
-    print("Not Found")
--- Lab1/send_email.py
+++
@@ -1,6 +0,0 @@
-from gmail import GMail, Message
-
-mail = GMail("htanh.c4e", "Hoilamgi@1")
-msg = Message("Xin nghỉ ốm", to="qhuydtvt@gmail.com",
-              text="Sáng nay dậy em phát hiện ra bị sốt")
-mail.send(msg)
